854639b15e1dda99e25f65fe172de28762ed65d2
José Eduardo García González <joseeduardogg@hotmail.com>
2020-12-31 22:18:48 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+//Services
+import { AuthModule, AuthGuard } from '@auth0/auth0-angular';
+
+//Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-zuxt79h5.us.auth0.com',
+      clientId: 'OqjQSg2nA9AxxSyHiVUSfn6mkfQ2eZyC',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
